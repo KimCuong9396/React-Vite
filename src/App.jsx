607e1aa1f47c30f6ec2ea5699b5c803a83cd7eec
KimCuong9396 <ttkcuong090396@gmail.com>
2025-02-16 17:09:49 +0700

    MyComponent

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const MyComponent = () => {
+  return <div> helllo cuong tieu ngu</div>;
+};
 const App = () => {
   const [count, setCount] = useState(0);
-  const myFunction = () => {
-    console.log(">>> run my arrow function");
-  };
-
-  myFunction();
   return (
     <>
       <div>
@@ -32,6 +30,7 @@ const App = () => {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <MyComponent></MyComponent>
     </>
   );
 };
