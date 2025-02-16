0eb75ce7b7514b27259bb15d3bc13148c7f0d47c
KimCuong9396 <ttkcuong090396@gmail.com>
2025-02-16 18:19:49 +0700

    jsx fragment

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,4 +1,13 @@
+import "./style.css";
+
 const MyComponent = () => {
-  return <div> helllo cuong tieu ngu</div>;
+  return (
+    <>
+      <div> helllo cuong tieu ngu</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        cuong tieu ngu oi
+      </div>
+    </>
+  );
 };
 export default MyComponent;
