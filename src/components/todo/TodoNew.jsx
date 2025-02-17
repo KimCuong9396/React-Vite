b342dd5eac64e80168543deda8d70f41d2d0b194
KimCuong9396 <ttkcuong090396@gmail.com>
2025-02-18 06:27:31 +0700

    event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,27 @@
 const TodoNew = (props) => {
   console.log(">> check props:", props);
   const { addNewTodo } = props;
-  addNewTodo("Kim Cuong cute qué chừng");
+  //addNewTodo("Kim Cuong cute qué chừng");
+  const handleClick = () => {
+    alert("click me");
+  };
+  // const handleOnchange = (event) => {
+  //   console.log(">>handle on change", event.target.value);
+  // };
+  const handleOnchange = (name) => {
+    console.log(">>handle on change", name);
+  };
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        /*onChange={handleOnchange}*/ onChange={(event) =>
+          handleOnchange(event.target.value)
+        }
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
