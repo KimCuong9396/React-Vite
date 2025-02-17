21d767648b64c84ef29f600a96dbf77669fc2572
KimCuong9396 <ttkcuong090396@gmail.com>
2025-02-17 21:54:08 +0700

    props function

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,4 +1,7 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+  console.log(">> check props:", props);
+  const { addNewTodo } = props;
+  addNewTodo("Kim Cuong cute qué chừng");
   return (
     <div className="todo-new">
       <input type="text" />
