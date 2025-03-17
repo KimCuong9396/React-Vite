131738ac2734e2af9399751aa2aa09fa4fb4fd54
KimCuong9396 <ttkcuong090396@gmail.com>
2025-03-17 16:51:42 +0700

    if

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,13 +1,17 @@
 const TodoData = (props) => {
-  const { name, age, data } = props;
-  console.log(">>> check props: ", props);
+  const { todoList} = props;
   return (
     <div className="todo-data">
-      <div>My name is {name} </div>
-      <div>Learning React</div>
-      <div>Watching Youtube</div>
-      <div>{JSON.stringify(props.todoList)}</div>
-    </div>
-  );
-};
+      {todoList.map((item, index)=>{
+        console.log(">>>check map:", item, index)
+        return (
+          <div className={`todo-item`} key={item.id}>
+            <div>{item.name}</div>
+            <button>Delete</button>
+          </div>
+        )
+      })}
+      </div>
+  )
+}
 export default TodoData;
