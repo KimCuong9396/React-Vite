a933f08452f373a8a43f706f25f96024f29eeaec
KimCuong9396 <ttkcuong090396@gmail.com>
2025-03-18 05:41:34 +0700

    deleteTodo

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ const App = () => {
     }
     setTodoList([...todoList, newTodo])
   };
+  const deleteTodo=(id)=>{
+    const newTodo=todoList.filter(item=>item.id !==id)
+    console.log(">>>check newTodo",newTodo)
+    setTodoList(newTodo)
+
+  }
   const  randomIntFromInterval=(min, max)=> { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
     }
@@ -25,7 +31,7 @@ const App = () => {
       <TodoNew addNewTodo={addNewTodo} />
 
       {todoList.length>0?
-      <TodoData todoList={todoList} />:
+      <TodoData todoList={todoList} deleteTodo={deleteTodo} />:
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>}
