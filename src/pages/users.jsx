71af84e5f0c8afc1022aef25da553b3880ab8404
KimCuong9396 <ttkcuong090396@gmail.com>
2025-03-18 12:07:45 +0700

    State for Form

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,4 +1,8 @@
+import UserTable from "../components/user/user.table";
+
 const UserPage=()=>{
-    return (<div>User page</div>)
+    return (<div style={{pading:"20px"}}>
+        <UserTable/>
+    </div>)
 }
 export default UserPage;
