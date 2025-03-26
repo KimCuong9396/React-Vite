eb4031330579b3a38f4e84d9c7d9508c61582940
KimCuong9396 <ttkcuong090396@gmail.com>
2025-03-26 09:34:44 +0700

    uncontrolled register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,58 @@
+import { Button, Form, Input } from "antd";
+
 const RegisterPage = () => {
-  return <div>Register page</div>;
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log(">>>check value", values);
+  };
+  return (
+    <>
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={onFinish}
+        //onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="FullName"
+          name="fullName"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Phone"
+          name="phone"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
+        {/* <button type="submit">Register</button> */}
+        <Button
+          type="primary"
+          onClick={() => {
+            form.submit();
+          }}
+        >
+          Register
+        </Button>
+      </Form>
+      {/* <Button type="primary">Register</Button> */}
+    </>
+  );
 };
 export default RegisterPage;
